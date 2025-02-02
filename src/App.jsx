8a892fdbfc413412832d0c42449120bbb7ce3c93
Karan bhuva <karanbhuva9861@gmail.com>
2025-02-02 20:15:45 +0530

    create company card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import UserCard from "./userCard/UserCard.jsx";
+import CompanyCard from "./ComapanyCard/index.jsx";
 const App = () => {
   return (
     <div>
       <UserCard />
+      <CompanyCard />
     </div>
   );
 };
