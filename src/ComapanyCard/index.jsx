2ef06c3319f56bc0ee85043e6d4d56cc7494eb57
Karan bhuva <karanbhuva9861@gmail.com>
2025-02-02 20:25:05 +0530

    incress length of compaines data

--- src/ComapanyCard/index.jsx
+++ src/ComapanyCard/index.jsx
@@ -1,49 +1,7 @@
 import React from "react";
 import "./companyCard.css";
-
+import { companies } from "../constantdata/constantdata.js";
 // Sample company data array
-const companies = [
-  {
-    id: 101,
-    name: "Tech Solutions Inc.",
-    location: "San Francisco, USA",
-    revenue: "$500M",
-    stockPrice: "$350",
-    international: "Yes",
-    offices: 15,
-    employees: "10,000+",
-  },
-  {
-    id: 102,
-    name: "Green Energy Ltd.",
-    location: "Berlin, Germany",
-    revenue: "$300M",
-    stockPrice: "$220",
-    international: "No",
-    offices: 10,
-    employees: "7,500",
-  },
-  {
-    id: 103,
-    name: "FinTech Corp.",
-    location: "London, UK",
-    revenue: "$750M",
-    stockPrice: "$500",
-    international: "Yes",
-    offices: 20,
-    employees: "12,000",
-  },
-  {
-    id: 104,
-    name: "AI Innovations",
-    location: "Tokyo, Japan",
-    revenue: "$1.2B",
-    stockPrice: "$650",
-    international: "Yes",
-    offices: 25,
-    employees: "15,000",
-  },
-];
 
 const CompanyCard = () => {
   return (
